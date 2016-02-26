15f1b7bb9d39f10e62ac73b0d2ac63230c1524f0
Ken Spencer <iota@electrocode.net>
2016-02-25 21:29:58 -0500

    Use Gen and Builder to start out

--- lib/gen.rb
+++ lib/gen.rb
@@ -3,13 +3,15 @@ require "builder"
 
 
 module Generator
-  #
+  # Base class on Builder to use HighLine
   class Inspircd < Builder
+    # Main InspIRCd interaction builder
     def self.main
       while agree("Want to make a block?", true)
         choose do |menu|
           menu.choice :conf, "Main Configuration" do |m|
 
+          end
           menu.init_help
         end
       end
@@ -17,6 +19,7 @@ module Generator
   end
   class Unrealircd
     def self.main
+      # ...
     end
   end
 end
